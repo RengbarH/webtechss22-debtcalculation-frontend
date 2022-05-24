6a42219deac8fbe851de798385ec8917d0d047cf
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-05-24 22:51:38 +0200

    Port auf 3000 gewechselt und Bootstrap implementiert

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+/* const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  port: '3000',
   transpileDependencies: true
-})
+}) */
+
+module.exports = {
+  devServer: {
+    port: 3000
+  }
+}
