18024458bf32e0a103f6ad9d7d5606e9dd714bc0
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-06-28 09:28:17 +0200

    NavBar "Home" Referenz durch "Who's the jerk" ersetzt

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Who's the jerk?</a>
+      <a class="navbar-brand" href="/">Who's the jerk?</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
               aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="/about">About</router-link>
           <router-link class="nav-link" to="/victim">Victim</router-link>
         </div>
